c8353b040effc5d2c0ee2e3449cc7e520026d8c0
Belito0157 <belitoxavier@gmail.com>
2022-12-09 23:29:52 +0100

    Error Handling

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, jsonify
 from flask_restful import Api, Resource
 from scrapping_service import ScrappingService 
+import logging
 
 app = Flask(__name__)
 api = Api(app)
 
+logging.basicConfig(level=logging.INFO, filename="api.log", filemode="w")
+
 class laptops(Resource):
     def get(self, brand):
-        scrapping_service = ScrappingService()
-        laptops = scrapping_service.scrape_all_laptops(brand)
-        return jsonify(laptops)
+        try:
+            scrapping_service = ScrappingService()
+            laptops = scrapping_service.scrape_all_laptops(brand)
+            return jsonify(laptops)
+        except Exception as exc:
+            raise Exception("Could not scrape the page.")
 
 api.add_resource(laptops, '/laptops/<brand>')
 
--- scrapping_service.py
+++ scrapping_service.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import logging
 
+logging.basicConfig(level=logging.INFO, filename="log.log", filemode="w")
 
 class ScrappingService():
 
@@ -12,15 +14,18 @@ class ScrappingService():
         }
 
     def scrape_page(self, page_url):
-        print(f"Started scrapping the page {page_url}...")
+        logging.info(f"Started scrapping the page {page_url}...")
         r = requests.get(page_url)
-        scrapped_page = BeautifulSoup(r.content, 'lxml')
-        print(f"Finished scrapping of the page {page_url}.")
-
-        return scrapped_page
+        if(r.status_code == 200):
+            scrapped_page = BeautifulSoup(r.content, 'lxml')
+            logging.info(f"Finished scrapping of the page {page_url}.")
+            return scrapped_page
+        else:
+            logging.error(f"Error happend on the server side, response code: {r.status_code}")
+            raise Exception(f"Error happend on the server side, response code: {r.status_code}.")
 
     def get_links_to_all_pages(self):
-        print("Getting links to all pages.")
+        logging.info("Getting links to all pages.")
         page_url = self.base_url + "test-sites/e-commerce/allinone/computers/laptops"
         scrapped_page = self.scrape_page(page_url)
         all_pages = scrapped_page.find_all('div', class_='caption')
@@ -30,52 +35,58 @@ class ScrappingService():
         for page in all_pages:
             for link in page.find_all('a', href=True):
                 all_pages_links.append(self.base_url + link['href'])
-        print("Finished getting links to all pages.")
+        logging.info("Finished getting links to all pages.")
 
         return all_pages_links
 
     def scrape_all_laptops(self, product_name):
-        print(f"Getting all the laptops of brand {product_name}")
+        logging.info(f"Getting all the laptops of brand {product_name}")
         laptops= []
         all_pages_links = self.get_links_to_all_pages()
 
 
         for link in all_pages_links:
-            print(f"Started scrapping the page {link}...")
+            logging.info(f"Started scrapping the page {link}...")
             scrapped_page = self.scrape_page(link)
-            print(f"Finished scrapping of the page {link}.")
+            logging.info(f"Finished scrapping of the page {link}.")
 
             try:
                 price = scrapped_page.find('h4', class_="pull-right price").text 
             except:
                 price = "0"
+                logging.warning("The price of this product is not available")
             try:
                 name = scrapped_page.find('h4', class_=None).text
             except:
                 name = "No name"
+                logging.warning("The name of this product is not available")
             try:    
                 description = scrapped_page.find('p', class_="description").text
             except:
                 description = "No description"
+                logging.warning("The description of this product is not available")
             try:
                 review = scrapped_page.select('p', class_=None)[5].text.strip()
             except:
                 review = "0"
+                logging.warning("The reviews of this product are not available")
             try:
-                items = [i.text for s in ['button.btn.swatch', 'button.btn.primary'] for i in scrapped_page.select(s)]
+                hdd = [i.text for s in ['button.btn.swatch', 'button.btn.primary'] for i in scrapped_page.select(s)]
             except:
-                items=[]
+                hdd=[]
+                logging.warning("The hdd's of this product are not available")
+
             laptop = {
                 'name': name,
                 'price': float(price.replace('$', '')),
                 'description': description,
                 'reviews': review,
-                'hdd': items
+                'hdd': hdd
             }
 
             laptops.append(laptop)
 
         laptops = [laptop for laptop in laptops if product_name in laptop['name']]
-        print(f"Getting all the laptops of brand {product_name}")
+        logging.info(f"Getting all the laptops of brand {product_name}")
 
         return sorted(laptops, key=lambda d: d['price']) 
