369d1a8d53306013e3dfc30d6ae021751ee1571f
Belito0157 <belitoxavier@gmail.com>
2022-12-13 03:51:48 +0100

    Used Selenium to be able to click buttons to have access to different price of the laptop when button is clicked

--- scrapping_service.py
+++ scrapping_service.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import logging
-
+from selenium import webdriver
 logging.basicConfig(level=logging.INFO, filename="log.log", filemode="w")
 
 class ScrappingService():
@@ -12,6 +12,7 @@ class ScrappingService():
         self.headers = {
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.42"
         }
+        self.path = "C:/Users/belit/Pictures/chromedriver"
 
     def scrape_page(self, page_url):
         logging.info(f"Started scrapping the page {page_url}...")
@@ -40,21 +41,35 @@ class ScrappingService():
         return all_pages_links
 
     def scrape_all_laptops(self, product_name):
+
+        op = webdriver.ChromeOptions()
+        op.add_argument('headless')
+        driver = webdriver.Chrome(self.path, options=op)
+        
         logging.info(f"Getting all the laptops of brand {product_name}")
         laptops= []
         all_pages_links = self.get_links_to_all_pages()
+        
 
 
         for link in all_pages_links:
+            lista_preco =[]
             logging.info(f"Started scrapping the page {link}...")
             scrapped_page = self.scrape_page(link)
             logging.info(f"Finished scrapping of the page {link}.")
+            driver.get(link)
+            
+            buttons = driver.find_elements_by_xpath("//button[contains(@class, 'btn swatch')]")
 
-            try:
-                price = scrapped_page.find('h4', class_="pull-right price").text 
-            except:
-                price = "0"
-                logging.warning("The price of this product is not available")
+            for button in buttons:
+                driver.execute_script("arguments[0].click();", button)
+                hdd = driver.find_element_by_xpath("//button[contains(@class, 'btn-primary')]").text                
+                valor_texto = driver.find_element_by_tag_name('h4').text
+                valor_float = float(valor_texto.replace('$', ''))
+                faxu = {hdd+'GB': valor_float}
+                lista_preco.append(faxu)
+                               
+
             try:
                 name = scrapped_page.find('h4', class_=None).text
             except:
@@ -70,23 +85,20 @@ class ScrappingService():
             except:
                 review = "0"
                 logging.warning("The reviews of this product are not available")
-            try:
-                hdd = [i.text for s in ['button.btn.swatch', 'button.btn.primary'] for i in scrapped_page.select(s)]
-            except:
-                hdd=[]
-                logging.warning("The hdd's of this product are not available")
-
+        
             laptop = {
                 'name': name,
-                'price': float(price.replace('$', '')),
+                'price': lista_preco,
                 'description': description,
-                'reviews': review,
-                'hdd': hdd
+                'reviews': review
             }
-
+            print(laptop)
             laptops.append(laptop)
 
+            
+        
         laptops = [laptop for laptop in laptops if product_name in laptop['name']]
         logging.info(f"Getting all the laptops of brand {product_name}")
+        
 
-        return sorted(laptops, key=lambda d: d['price']) 
+        return laptops
